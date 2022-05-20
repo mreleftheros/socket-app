73005899dcaf4b2aecbccb59feefb22f7c2ee89e
eleftheros <eleftheros_all@protonmail.com>
2022-05-20 15:36:42 +0300

    Add path for ui

--- api/app.js
+++ api/app.js
@@ -12,4 +12,13 @@ app.use(cookieParser());
 
 app.use('/api/auth', auth);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../ui/public')));
+  app.get('*', (req, res) => {
+    return res.sendFile(
+      path.resolve(__dirname, '../', 'ui', 'public', 'index.html')
+    );
+  });
+}
+
 module.exports = app;
